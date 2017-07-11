d5de916db3acd8e0556b694364e9365f8e53c9fc
Ryan Ferguson <fergusman@gmail.com>
2017-07-11 13:53:09 -0400

    added reducers

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App.react.js';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import reducer from './reducers'
 import './stylesheets/index.css'
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-
-  <Router>
-    <Route path="/" component={App} />
-  </Router>, document.getElementById('root'));
-
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>, document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import postDisplay from './postDisplay'
+
+
+const blogApp = combineReducers({
+  postDisplay
+})
+
+export default blogApp
