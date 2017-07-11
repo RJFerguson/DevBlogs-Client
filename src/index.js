1f9866266fe1c7813a8aef17c189bd75a5b9191d
Ryan Ferguson <fergusman@gmail.com>
2017-07-11 13:27:24 -0400

    added redux packages switching to new branch now

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM,{ render } from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App.react.js';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './stylesheets/index.css'
 
-ReactDOM.render(<Router>
+ReactDOM.render(
+
+  <Router>
     <Route path="/" component={App} />
   </Router>, document.getElementById('root'));
 
